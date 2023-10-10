cb6d4dcd9d05ed30748a9cc1a12232348aff756c
Soraya Artiel <artielsoraya@gmail.com>
2023-10-11 00:42:45 +0200

    animation added

--- app/src/main/java/com/art/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/art/horoscapp/ui/luck/LuckFragment.kt
@@ -1,15 +1,19 @@
 package com.art.horoscapp.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.art.horoscapp.R
-import com.art.horoscapp.databinding.FragmentHoroscopeBinding
 import com.art.horoscapp.databinding.FragmentLuckBinding
-import com.art.horoscapp.ui.horoscope.HoroscopeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,8 +22,46 @@ class LuckFragment : Fragment() {
 
     private val luckViewModel by viewModels<LuckViewModel>()
 
-    private  var _binding: FragmentLuckBinding? = null
+    private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ivRoulette.setOnClickListener { spinRoulette() }
+    }
+
+    private fun spinRoulette() {
+        val random = java.util.Random()
+        val degrees = random.nextInt(1440) + 360
+
+        val animator = ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, degrees.toFloat())
+        binding.card.isVisible = false
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd { slideCard() }
+        animator.start()
+    }
+
+    private fun slideCard() {
+        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
+
+        slideUpAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                binding.card.isVisible = true
+            }
+            override fun onAnimationEnd(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+        binding.card.startAnimation(slideUpAnimation)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
