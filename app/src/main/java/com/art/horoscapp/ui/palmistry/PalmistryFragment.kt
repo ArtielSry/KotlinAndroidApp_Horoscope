014adbd988c3c267af2808fbe91fbd8abf26aa98
Soraya Artiel <artielsoraya@gmail.com>
2023-10-12 11:32:43 +0200

    testing

--- app/src/main/java/com/art/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/art/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -1,16 +1,18 @@
 package com.art.horoscapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
-import com.art.horoscapp.Manifest
-import com.art.horoscapp.R
-import com.art.horoscapp.databinding.FragmentLuckBinding
 import com.art.horoscapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -40,13 +42,35 @@ class PalmistryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         if (checkCameraPermission()) {
-
+            startCamera()
         } else {
             requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
         }
     }
 
-    fun checkCameraPermission(): Boolean {
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e:Exception) {
+                Log.e("Error here:", "${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+
+    private fun checkCameraPermission(): Boolean {
         return PermissionChecker.checkSelfPermission(
             requireContext(),
             android.Manifest.permission.CAMERA
